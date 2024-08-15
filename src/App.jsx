5869315d65e55bb036b5656604a8e6c34a67104e
erandiiii <erandberisha1@gmail.com>
2024-08-15 12:30:49 -0700

    asd

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Nav from './Nav'
+import Home from './components/Home/Home'
+import CurvedTxt from './components/CurvedTxt'
+import Footer from './components/Footer/Footer'
+import { Route, Routes } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop'
+// import Models from './components/Models'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ScrollToTop/>
+      <Nav/>
+       <Home/>
+      <Routes>
+        
+      </Routes>
+      
+      
+      <Footer/> 
+      
+      
+    </>
+  )
+}
+
+export default App
